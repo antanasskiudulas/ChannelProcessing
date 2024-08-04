1b36151bef0313b79467e36c14da5aad89353835
Vocal <antanas.skiudulas@outlook.com>
2024-08-04 15:00:54 +0100

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,10 @@
+﻿namespace ChannelProcessing
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, World!");
+        }
+    }
+}
